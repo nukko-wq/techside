2e7b9e8e7f3398636eb13dda09f1b186c9d33564
nukko <shouta.tsuboi@gmail.com>
2024-10-04 10:26:59 +0900

    Linkの追加

--- app/components/layouts/Header/Header.tsx
+++ app/components/layouts/Header/Header.tsx
@@ -1,8 +1,9 @@
+import Link from 'next/link'
 const Header = () => {
 	return (
 		<header className='px-4 py-2 sticky top-0 backdrop-blur-[5px] shadow-custom z-50 border-b border-slate-200'>
 			<div className='text-slate-600 text-2xl font-bold text-center md:text-left'>
-				Tech Side
+				<Link href='/'>Tech Side</Link>
 			</div>
 		</header>
 	)
