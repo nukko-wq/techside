42ebe755dde9b33b61c4e5661aa1a3dc63a345a0
nukko <shouta.tsuboi@gmail.com>
2024-10-05 10:17:50 +0900

    スタイルの調整

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
 			>
 				<div className='flex flex-col min-h-screen bg-gray-50'>
 					<Header />
-					{children}
+					<main className='flex-grow'>{children}</main>
 					<Footer />
 				</div>
 			</body>
--- app/page.tsx
+++ app/page.tsx
@@ -3,37 +3,37 @@ import QiitaArticle from '@/app/features/qiita/components/QiitaArticle'
 
 export default function Home() {
 	return (
-		<div className='flex-grow px-4 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 py-4 max-w-7xl lg:max-w-full mx-auto'>
-			<div className='rounded-md bg-slate-100'>
+		<div className='px-4 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 py-4 max-w-7xl lg:max-w-full mx-auto overflow-hidden'>
+			<div className='flex flex-col rounded-md h-[calc(100vh-theme(spacing.32))]'>
 				<div className='flex justify-center bg-slate-600 text-white rounded-t-md'>
 					<h2 className='text-2xl font-bold'>Zenn Tech Trend</h2>
 				</div>
-				<div className='p-4'>
+				<div className='p-4 bg-slate-100 rounded-b-md overflow-y-auto'>
 					<Article apiUrl='https://zenn-api.vercel.app/api/trendTech' />
 				</div>
 			</div>
-			<div className='flex flex-col rounded-md'>
+			<div className='flex flex-col rounded-md h-[calc(100vh-theme(spacing.32))]'>
 				<div className='flex justify-center bg-slate-600 text-white rounded-t-md'>
 					<h2 className='text-2xl font-bold'>Zenn Ideas Trend</h2>
 				</div>
-				<div className='p-4 bg-slate-100 rounded-b-md'>
+				<div className='p-4 bg-slate-100 rounded-b-md overflow-y-auto'>
 					<Article apiUrl='https://zenn-api.vercel.app/api/trendIdea' />
 				</div>
 			</div>
-			<div className='flex flex-col rounded-md'>
+			<div className='flex flex-col rounded-md h-[calc(100vh-theme(spacing.32))]'>
 				<div className='flex justify-center bg-slate-600 text-white rounded-t-md'>
 					<h2 className='text-2xl font-bold'>Zenn Books Trend</h2>
 				</div>
-				<div className='p-4 bg-slate-100 rounded-b-md'>
+				<div className='p-4 bg-slate-100 rounded-b-md h- overflow-y-auto'>
 					<Article apiUrl='https://zenn-api.vercel.app/api/trendBook' />
 				</div>
 			</div>
 			{/* Qiitaの記事を表示する */}
-			<div className='flex flex-col rounded-md'>
+			<div className='flex flex-col rounded-md h-[calc(100vh-theme(spacing.32))]'>
 				<div className='flex justify-center bg-slate-600 text-white rounded-t-md'>
 					<h2 className='text-2xl font-bold'>Qiita Trend</h2>
 				</div>
-				<div className='p-4 bg-slate-100 rounded-b-md'>
+				<div className='p-4 bg-slate-100 rounded-b-md overflow-y-auto'>
 					<QiitaArticle />
 				</div>
 			</div>
