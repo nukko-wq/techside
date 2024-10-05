a4a9365f17855ae579e029585058ea0b8c4ed556
nukko <shouta.tsuboi@gmail.com>
2024-10-05 11:12:47 +0900

    ローディング、Spinnerの追加

---
+++ app/components/elements/Spinner/Spinner.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Spinner = () => {
+	return (
+		<div className='flex flex-grow justify-center items-center gap-6'>
+			<div className='animate-spin rounded-full h-10 w-10 border-[5px] border-t-transparent border-blue-500' />
+			<p className='text-lg font-bold text-blue-500'>Loading...</p>
+		</div>
+	)
+}
+
+export default Spinner
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,21 @@
-import Article from '@/app/features/zenn/components/Article'
-import QiitaArticle from '@/app/features/qiita/components/QiitaArticle'
+import Spinner from '@/app/components/elements/Spinner/Spinner'
+import dynamic from 'next/dynamic'
+
+const DynamicArticle = dynamic(
+	() => import('@/app/features/zenn/components/Article'),
+	{
+		loading: () => <Spinner />,
+		ssr: false,
+	},
+)
+
+const DynamicQiitaArticle = dynamic(
+	() => import('@/app/features/qiita/components/QiitaArticle'),
+	{
+		loading: () => <Spinner />,
+		ssr: false,
+	},
+)
 
 export default function Home() {
 	return (
@@ -8,24 +24,24 @@ export default function Home() {
 				<div className='flex justify-center bg-slate-600 text-white rounded-t-md'>
 					<h2 className='text-2xl font-bold'>Zenn Tech Trend</h2>
 				</div>
-				<div className='p-4 bg-slate-100 rounded-b-md overflow-y-auto'>
-					<Article apiUrl='https://zenn-api.vercel.app/api/trendTech' />
+				<div className='p-4 bg-slate-100 rounded-b-md overflow-y-auto flex flex-grow'>
+					<DynamicArticle apiUrl='https://zenn-api.vercel.app/api/trendTech' />
 				</div>
 			</div>
 			<div className='flex flex-col rounded-md h-[calc(100vh-theme(spacing.28))]'>
 				<div className='flex justify-center bg-slate-600 text-white rounded-t-md'>
 					<h2 className='text-2xl font-bold'>Zenn Ideas Trend</h2>
 				</div>
-				<div className='p-4 bg-slate-100 rounded-b-md overflow-y-auto'>
-					<Article apiUrl='https://zenn-api.vercel.app/api/trendIdea' />
+				<div className='p-4 bg-slate-100 rounded-b-md overflow-y-auto flex flex-grow'>
+					<DynamicArticle apiUrl='https://zenn-api.vercel.app/api/trendIdea' />
 				</div>
 			</div>
 			<div className='flex flex-col rounded-md h-[calc(100vh-theme(spacing.28))]'>
 				<div className='flex justify-center bg-slate-600 text-white rounded-t-md'>
 					<h2 className='text-2xl font-bold'>Zenn Books Trend</h2>
 				</div>
-				<div className='p-4 bg-slate-100 rounded-b-md h- overflow-y-auto'>
-					<Article apiUrl='https://zenn-api.vercel.app/api/trendBook' />
+				<div className='p-4 bg-slate-100 rounded-b-md h- overflow-y-auto flex flex-grow'>
+					<DynamicArticle apiUrl='https://zenn-api.vercel.app/api/trendBook' />
 				</div>
 			</div>
 			{/* Qiitaの記事を表示する */}
@@ -33,8 +49,8 @@ export default function Home() {
 				<div className='flex justify-center bg-slate-600 text-white rounded-t-md'>
 					<h2 className='text-2xl font-bold'>Qiita Trend</h2>
 				</div>
-				<div className='p-4 bg-slate-100 rounded-b-md overflow-y-auto'>
-					<QiitaArticle />
+				<div className='p-4 bg-slate-100 rounded-b-md overflow-y-auto flex flex-grow'>
+					<DynamicQiitaArticle />
 				</div>
 			</div>
 		</div>
