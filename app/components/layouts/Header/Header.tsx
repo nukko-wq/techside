96a99c077c8bd71ef9a2dccc8e850d7b7a309b90
nukko <shouta.tsuboi@gmail.com>
2024-10-08 18:12:17 +0900

    Headerのリンクを修正

--- app/components/layouts/Header/Header.tsx
+++ app/components/layouts/Header/Header.tsx
@@ -1,9 +1,17 @@
+'use client'
+
 import Link from 'next/link'
+import { useFilter } from '@/app/features/filter/components/Filter'
+
 const Header = () => {
+	const { setFilter } = useFilter()
+
 	return (
 		<header className='px-4 py-2 sticky top-0 backdrop-blur-[5px] shadow-custom z-50 border-b border-slate-200'>
 			<div className='text-slate-600 text-2xl font-bold text-center md:text-left'>
-				<Link href='/'>Tech Side</Link>
+				<Link href='/' onClick={() => setFilter('all')}>
+					Tech Side
+				</Link>
 			</div>
 		</header>
 	)
