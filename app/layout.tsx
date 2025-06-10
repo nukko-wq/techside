d81c2512d74b2d23ce4e3f4c2e4429b6399a9a56
nukko <shouta.tsuboi@gmail.com>
2025-06-10 18:24:11 +0900

    不要なフォントを削除

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,8 @@
 import type { Metadata } from 'next'
-import localFont from 'next/font/local'
 import './globals.css'
 import Footer from '@/app/components/layouts/Footer/Footer'
 import Header from '@/app/components/layouts/Header/Header'
 import { FilterProvider } from '@/app/features/filter/components/Filter'
-
-const geistSans = localFont({
-	src: './fonts/GeistVF.woff',
-	variable: '--font-geist-sans',
-	weight: '100 900',
-})
-const geistMono = localFont({
-	src: './fonts/GeistMonoVF.woff',
-	variable: '--font-geist-mono',
-	weight: '100 900',
-})
 
 export const metadata: Metadata = {
 	title: 'Tech Side',
@@ -32,9 +20,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='ja'>
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
+			<body>
 				<FilterProvider>
 					<div className='flex flex-col min-h-screen bg-gray-50'>
 						<Header />
